99f1645d2f5ddde5259b56cb2bf1db2dfd30bb57
Akın Kürşat Özkan <aknkrstozkn@localhost.localdomain>
2020-10-22 23:53:04 +0300

    Add new Player movement

--- Assets/PlayerCharacter/PlayerController.cs
+++ Assets/PlayerCharacter/PlayerController.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _speed = 0.5f;
-   // [SerializeField] Vector2 _clampValues = new Vector2(-12, 12);
+    [SerializeField] float _rotSpeed = 5f;
     private Rigidbody _rb;
     private Vector3 _targetPos;
+    private Vector3 _lookAtTarget;
+    private Quaternion _playerRot;
     private Vector3 _startPos;
-    private Touch _touch;
+    /* Mobile Control
+    private Touch _touch; */
     private string _currentState = "Idle";
     private string _newState = "Idle";
     void Start()
     {
-        Input.simulateMouseWithTouches = true;
-
         _rb = GetComponent<Rigidbody>();
         _startPos = transform.position;
         _targetPos = transform.position;
@@ -23,7 +24,14 @@ public class PlayerController : MonoBehaviour
 
     
     void Update()
-    {   /*
+    {   
+        InputControl();
+
+        Move();        
+    }
+    void InputControl()
+    {
+        /* Mobile Control, for only mobile because this is more efficient
         if (Input.touchCount > 0)
         {   
             _touch = Input.GetTouch(0);
@@ -35,24 +43,21 @@ public class PlayerController : MonoBehaviour
             }
         }
         */
-        if(Input.GetMouseButton(0)) 
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetTargetPosition();
+        }
+        else if(Input.GetMouseButton(0)) 
         {
             if(Input.GetAxisRaw("Mouse X") != 0 || Input.GetAxisRaw("Mouse Y") != 0)
             {
                 SetTargetPosition();
             }
         }
-                 
-
-        transform.position = Vector3.MoveTowards(transform.position, _targetPos, _speed * Time.deltaTime);
-
-        float distance = Vector3.Distance(_targetPos, transform.position);
-        _newState = distance < 0.1f ? "Idle" : "Running"; 
     }
-
     void SetTargetPosition()
-    {
-        //var ray = Camera.main.ScreenPointToRay(_touch.position);
+    {   /* Mobile control
+        var ray = Camera.main.ScreenPointToRay(_touch.position); */
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 1000))
@@ -60,13 +65,28 @@ public class PlayerController : MonoBehaviour
             if (hit.transform != null && hit.transform.tag != "Player")
             {
                 _targetPos = hit.point;
-                transform.LookAt(_targetPos);
+                //transform.LookAt(_targetPos);
+                _lookAtTarget = new Vector3(_targetPos.x - transform.position.x, 
+                    transform.position.y,
+                    _targetPos.z - transform.position.z);
+                _playerRot = Quaternion.LookRotation(_lookAtTarget);
             }
         }
+    }
+    void Move()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, 
+            _playerRot, _rotSpeed * Time.deltaTime);
+        
+        transform.position = Vector3.MoveTowards(transform.position, _targetPos, _speed * Time.deltaTime);
+
     }
 
     void ChangeAnimState() 
     {
+        float distance = Vector3.Distance(_targetPos, transform.position);
+        _newState = distance < 0.1f ? "Idle" : "Running";
+
         if (_currentState == _newState) return;
 
         _currentState = _newState;
@@ -80,6 +100,6 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //_newState = _rb.velocity.magnitude < 0.1f ? "Idle" : "Running";        
+        // No Physic calc yet.     
     }
 }
